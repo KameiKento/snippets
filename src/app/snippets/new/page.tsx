304f620fa71f00b48455b3fd96429affdd487f0d
KameiKento <kento1024kk@gmail.com>
2024-03-15 21:37:41 +0900

    スタイリングまで完了

---
+++ src/app/snippets/new/page.tsx
@@ -0,0 +1,56 @@
+import { redirect } from "next/navigation";
+import { db } from "@/db";
+
+export default function SnippetCreatePage() {
+  async function createSnippet(formData: FormData) {
+    // server actionであることを明記する
+    "use server";
+    // userからのinputをチェックし、validであることを確認する
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+    // detabaseに新しいレコードを追加する
+    const snippet = await db.snippet.create({
+      data: {
+        title: title,
+        code: code,
+      },
+    });
+
+    // rootにリダイレクト
+    redirect("/");
+  }
+
+  return (
+    <form action={createSnippet}>
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">
+            Title
+          </label>
+          <input
+            type="text"
+            name="title"
+            className="border rounded p-2 w-full"
+            id="title"
+          />
+        </div>
+
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">
+            Code
+          </label>
+          <textarea
+            name="code"
+            className="border rounded p-2 w-full"
+            id="code"
+          />
+        </div>
+
+        <button type="submit" className="rounded p-2 bg-blue-200">
+          Create
+        </button>
+      </div>
+    </form>
+  );
+}
